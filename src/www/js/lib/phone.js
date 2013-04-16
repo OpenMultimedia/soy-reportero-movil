c7cbdf7203fce32e15e0bc708e1b189bce339109
Gonzalo Almeida <flecox@gmail.com>
2013-04-15 23:34:11 -0300

    agregamos postear imagen

--- src/www/js/lib/phone.js
+++ src/www/js/lib/phone.js
@@ -1,4 +1,4 @@
- var pictureSource;   // picture source
+  var pictureSource;   // picture source
   var destinationType; // sets the format of returned value
 
   // Wait for Cordova to connect with the device
@@ -15,16 +15,10 @@
   // Called when a photo is successfully retrieved
   //
 
-	function win(r) {
-			alert(r.responseCode);
-			alert(r.response);
-          console.log("Code = " + r.responseCode);
-          console.log("Response = " + r.response);
-          console.log("Sent = " + r.bytesSent);
-          console.log(r);
-          console.log("suuubiiooo");
-          $("#file-id").val(r.response['id']);
-          $("#file-id").trigger("change");
+    function win(r) {
+          var response = JSON.parse(r.response);
+          $("#file-id-input").val(response['id']);
+          $("#file-id-input").trigger("change");
       }
 
   function fail(error) {
@@ -34,99 +28,74 @@
       }
 
    function uploadFile(imageURI) {
-   		var options = new FileUploadOptions();
+        var options = new FileUploadOptions();
           options.fileKey="file";
           options.chunkedMode = false;
           options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
           var params = {};
           options.params = params;
           var ft = new FileTransfer();
+          ft.onprogress = function (progressEvent) {
+               if (progressEvent.lengthComputable) {
+                   percentLoaded = Math.round(100 * (progressEvent.loaded / progressEvent.total));
+                   console.log(percentLoaded);
+                   $(".progressbar-container").css("display", "block");
+                   $(".progressbar").css("width", percentLoaded + "%");
+                   if(percentLoaded >= 99) {
+                    $(".progresstext").text("Archivo subido exitosamente");
+                   }
+               }
+           };
           ft.upload(imageURI, encodeURI("https://upload.openmultimedia.biz/files/"), win, fail, options, true);
   }
 
   function checkConnectionAndSizeAndUpload(imageURI) {
-  	console.log("nuuuu");
-  	var states = {};
-  	states[Connection.UNKNOWN]  = 'Unknown connection';
-  	states[Connection.ETHERNET] = 'Ethernet connection';
-  	states[Connection.WIFI]     = 'WiFi connection';
-  	states[Connection.CELL_2G]  = 'Cell 2G connection';
-  	states[Connection.CELL_3G]  = 'Cell 3G connection';
-  	states[Connection.CELL_4G]  = 'Cell 4G connection';
-  	states[Connection.NONE]     = 'No network connection';
-  	console.log("hhhhheeey");
-  	//resolve file check size and upload
-  	window.resolveLocalFileSystemURI(imageURI, function(fileEntry) {
-  		console.log("hoooooo");
-      	fileEntry.file(function(fileObj) {
-      		var networkState = navigator.network.connection.type;
-      		alert("you are using: " + states[networkState]);
-  			alert("the file you are trying to upload is: " + fileObj.size + "bytes");
-  			uploadFile(imageURI);
-
-
-      	});
-  	});
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.NONE]     = 'No network connection';
+    //resolve file check size and upload
+    window.resolveLocalFileSystemURI(imageURI, function(fileEntry) {
+        fileEntry.file(function(fileObj) {
+            var networkState = navigator.network.connection.type;
+            console.log("muaajajajajaj");
+            $(".conextion-type").text("Esta usando: " + states[networkState] + " y el archivo que desea cargar pesa: " + fileObj.size + "bytes, ¿desea continuar?");
+            $("#acept-upload-button").click( function() {
+                uploadFile(imageURI);
+            });
+            $("#dialogstarter").trigger("click");
+        });
+    });
 
   }
-
-
-  function onPhotoDataSuccess(imageData) {
-    // Uncomment to view the base64 encoded image data
-    // console.log(imageData);
-
-    // Get image handle
-    //
-    var smallImage = document.getElementById('smallImage');
-
-    // Unhide image elements
-    //
-    smallImage.style.display = 'block';
-
-    // Show the captured photo
-    // The inline CSS rules are used to resize the image
-    //
-    smallImage.src = "data:image/jpeg;base64," + imageData;
-  }
-
-
-
 
   // Called when a photo is successfully retrieved
   //
-  function onPhotoURISuccess(imageURI) {
-    console.log("quepasa aca");
-    // Uncomment to view the image file URI
-    console.log(imageURI);
-  checkConnectionAndSizeAndUpload(imageURI);
-    // Get image handle
-    //
-    var largeImage = document.getElementById('largeImage');
-
-
-  }
+function onPhotoURISuccess(imageURI) {
+    console.log("ando");
+    checkConnectionAndSizeAndUpload(imageURI);
+}
 
   // A button will call this function
   //
   function capturePhoto() {
     // Take picture using device camera and retrieve image as base64-encoded string
-    console.log("manshana");
     navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
       destinationType: navigator.camera.DestinationType.FILE_URI});
-   console.log("meeeesssi");
   }
 
 
   // capture callback
 var captureSuccess = function(mediaFiles) {
-console.log("hhheeey");
-console.log(mediaFiles);
   var i, path, len;
   for (i = 0, len = mediaFiles.length; i < len; i += 1) {
       path = mediaFiles[i].fullPath;
       console.log(path);
       onPhotoURISuccess(path);
-      // do something interesting with the file
   }
 };
 
@@ -156,15 +125,3 @@ var captureError = function(error) {
   function onFail(message) {
     alert('Failed because: ' + message);
   }
-
-  var showConection = function() {
-  	var Videos = Backbone.Collection.extend({
-	   url:'http://multimedia.tlsur.net/api/clip/'
-});
-console.log("guuau");
-	console.log("miaauu");
-	var video = new Videos();
-	var result = video.fetch();
-	console.log(result);
-	console.log("yoursister");
-};
--- src/www/js/main.js
+++ src/www/js/main.js
@@ -1,63 +1,172 @@
+var initial = window.location.href;
+
 function getReports(offset, batchSize, callback) {
-	$.getJSON('http://multimedia.tlsur.net/api/clip/?ultimo=10&tipo=soy-reportero&autenticado=w3bt3l3sUrTV',
+    $.getJSON('http://multimedia.tlsur.net/api/clip/?ultimo=10&tipo=soy-reportero&autenticado=w3bt3l3sUrTV',
     function(data) {
-		$.each(data, function(key, val) {
-    		console.log(key);
-  		});
-  		callback(data);
-	 });
-	//console.log(videos.fetch({ dataType: 'jsonp' }));
+        callback(data);
+     });
+    //console.log(videos.fetch({ dataType: 'jsonp' }));
 }
 
+function sorted_keys(obj) {
+    var keys = [];
+    for(var key in obj)
+    {
+        if(obj.hasOwnProperty(key))
+        {
+            keys.push(key);
+        }
+    }
+    return keys.sort();
+}
 
-console.log("no");
+function cleanForm() {
+    $(".progressbar-container").css("display","none");
+    $(".progressbar").css("width", "0%");
+    $("#name-input").val("");
+    $("#title-input").val("");
+    $("#description-input").val("");
+    $("#description-input").text("");
+    $("#file-id-input").val("");
+    $("#send-button").remove();
+}
+
+function sing_request(params_dict, key, secret) {
+    params_dict['key'] = key;
+    cadena = secret;
+    var i = 0;
+    var sorted_k = sorted_keys(params_dict);
+    for(i=0; i<sorted_k.length; i++) {
+      var new_key = sorted_k[i];
+      cadena += new_key + params_dict[new_key];
+    }
+    return md5(cadena);
+}
+
+function post_report() {
+    var data = {'tipo': 'soy-reportero'};
+    var name = $("#name-input").val();
+    if(name) {
+        data['name'] = name;
+    }
+    var description = $("#description-input").val();
+    if(description) {
+        data['report'] = description;
+    }
+    var file_id = $("#file-id-input").val();
+    if(file_id) {
+        data['archivo'] = name;
+    }
+    security_key = 'k4}"-^30C$:3l04$(/<5"7*6|Ie"6x';
+    key = 'telesursoyreporteroplonepruebas';
+    var signature = sing_request(data, key, security_key);
+    data['signature'] = signature;
+    console.log(data);
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/x-www-form-urlencoded',
+        //accepts: 'application/json; charset=UTF-8',
+
+        url: 'http://multimedia.tlsur.net/api/imagen/',
+        dataType: "json",
+        data: data,
+        success: function(dato, textStatus, jqXHR){
+            console.log(dato);
+            console.log(textStatus);
+            console.log(jqXHR);
+            var response = JSON.parse(jqXHR['responseText']);
+            var slug = response['slug'];
+            cleanForm();
+            $("#acept-success-button").click(function() {
+                window.location.href = initial + "#page1";
+            });
+            $("#dialogreportsucess").trigger("click");
+            $("#send-button").remove();
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
+}
+
+function checkReport() {
+    console.log("mongoo");
+    console.log(initial);
+    if($("#send-button").length) {
+        $("#acept-create-button").click(function() {
+           cleanForm();
+            //$("#check-report").trigger("click");
+        });
+        $("#edit-create-button").click(function() {
+            window.location.href = initial + "#formPage";
+            //$("#edit-report").trigger("click");
+        });
+        $("#dialogexist").trigger("click");
+    } else {
+        window.location.href = initial + "#createReport";
+        //$("#check-report").trigger("click");
+    }
+}
+
 $(document).ready(
-		function() {
-			console.log("your sister");
-			var report_list = {};
-			var selected_slug;
-			$( 'div' ).on( 'pagehide',function(event, ui) {
-				if( ui.nextPage[0].id === "listPage") {
-					console.log("esta");
-					getReports(0, 4, function(data) {
-						var i = 0;
-						var html = "";
-						for(i=0; i< data.length; i++) {
-							var report = data[i];
-							report_list[report.slug] = report;
-							var title = report.titulo;
-							var slug = report.slug;
-							var descripcion = report.descripcion;
-							var thumbnail = report.thumbnail_pequeno;
-							html += "<li class='report-item-li' ><a href='#showReport' class='report-list-item' data-transition='slide' data-slug='"+slug+"'><img src='" + thumbnail + "' /><span class='title'>"+ title +"</span></a></li>";
-							$("#list-reports").html(html);
-							$(".report-list-item").click(function() {
-								selected_slug = $(this).attr("data-slug");
-							});
-						}
-					});
-				}
-				else if(ui.nextPage[0].id === "showReport") {
-					var isVideo = true;
-					var report = report_list[selected_slug];
-					$("#report-description").text(report.descripcion);
-					if(isVideo) {
-						console.log(report.slug);
-						$("#report-media-container").omplayer({
-							slug: report.slug,
-							width: $("#report-media-container").width(),
-							height: 255
-						});
-					} else {
-						$("#report-img").attr("src", report.thumbnail_grande);
-					}
-				}
-			});
+        function() {
+            console.log("your sister");
 
-			$(".report-list-item").on("tap", function() {
-				console.log($(this).attr("data-slug"));
-				console.log("hee");
+            var report_list = {};
+            var selected_slug;
+            $( 'div' ).on( 'pagehide',function(event, ui) {
+                if( ui.nextPage[0].id === "listPage") {
+                    console.log("esta");
+                    getReports(0, 4, function(data) {
+                        var i = 0;
+                        var html = "";
+                        for(i=0; i< data.length; i++) {
+                            var report = data[i];
+                            report_list[report.slug] = report;
+                            var title = report.titulo;
+                            var slug = report.slug;
+                            var descripcion = report.descripcion;
+                            var thumbnail = report.thumbnail_pequeno;
+                            html += "<li class='report-item-li' ><a href='#showReport' class='report-list-item' data-transition='slide' data-slug='"+slug+"'><img src='" + thumbnail + "' /><span class='title'>"+ title +"</span></a></li>";
+                            $("#list-reports").html(html);
+                            $(".report-list-item").click(function() {
+                                selected_slug = $(this).attr("data-slug");
+                            });
+                        }
+                    });
+                }
+                else if(ui.nextPage[0].id === "showReport") {
+                    var isVideo = true;
+                    var report = report_list[selected_slug];
+                    $("#report-description").text(report.descripcion);
+                    if(isVideo) {
+                        console.log(report.slug);
+                        $("#report-media-container").omplayer({
+                            slug: report.slug,
+                            width: $("#report-media-container").width(),
+                            height: 255
+                        });
+                    } else {
+                        $("#report-img").attr("src", report.thumbnail_grande);
+                    }
+                }
+            });
 
-			});
-		}
-	);
+            $("#file-id-input").on("change", function() {
+                console.log("cambio");
+                $("#send-button-container").html('<a data-role="button" id="send-button" data-icon="check">Enviar Reporte</a>');
+                $("#send-button-container").trigger("create");
+                $("#send-button").on("click", function() {
+                    post_report();
+                });
+            });
+
+            $(".report-list-item").on("tap", function() {
+                console.log($(this).attr("data-slug"));
+                console.log("hee");
+
+            });
+        }
+    );
